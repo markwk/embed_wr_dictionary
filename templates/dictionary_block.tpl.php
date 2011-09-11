98090c038b40f1a073d7226917a93d8f33c811ac
Mark Koester <mwkteacher@gmail.com>
2011-09-11 21:11:04 +0800

    initial commit

---
+++ templates/dictionary_block.tpl.php
@@ -0,0 +1,6 @@
+<div class="wr_dictionary">
+  <a href="javascript:void(0);" id="search-dictionary" style="font-weight:bold;">Search Dictionary</a>
+  <div id="wr_content" style="display:none;">
+    <iframe height="320" name="WRmini" src="http://mini.wordreference.com/mini/index.aspx?dict=<?php print $dictionary; ?>&amp;w=start&amp;u=<?php print $userid; ?>" width="320"></iframe>
+  </div>
+</div>
