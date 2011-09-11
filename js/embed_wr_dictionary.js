289c9cb39df23370431dd18687c4d0310546ab3b
Mark Koester <mwkteacher@gmail.com>
2011-09-12 02:50:25 +0800

    Edited Search Dictionary for Words

--- js/embed_wr_dictionary.js
+++ js/embed_wr_dictionary.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   $('#search-dictionary').click(function() {
     $('#wr_content').slideToggle('slow');
-    if ($('#search-dictionary').html() =='Search Dictionary') {
+    if ($('#search-dictionary').html() =='Search Dictionary for Words') {
       $('#search-dictionary').html('Hide Dictionary'); 
     }
     else {
-      $('#search-dictionary').html('Search Dictionary');  
+      $('#search-dictionary').html('Search Dictionary for Words');  
     }
   });
 });
